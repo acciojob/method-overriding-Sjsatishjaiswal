790f6b37069938a3fa1f86c25f69991a3087bfbc
Satish Jaiswal <Sjsatishjaiswalcse2017@gmail.com>
2023-03-16 01:06:06 +0530

    Assignment of method

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,21 @@
 package com.driver;
 
 public class Main {
+    public static class A{
+        public String meth(){
+            return  "Invoking method from class A";
+        }
+    }
+    public  static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B" ;
+        }
+
+    }
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println(obj.meth()); // will output "Method is overridden in Extended class B"
-        System.out.println(((A)obj).meth()); // will output "Invoking method from class A"
+        obj.meth(); // will output "Method is overridden in Extended class B"
+        B obj1 =new B(); // will output "Invoking method from class A"
+        obj1.meth();
     }
 }
